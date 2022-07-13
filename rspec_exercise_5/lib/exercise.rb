29681d928dc4ec4bc227ea1e2f957aad9d51ca19
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-07-12 19:49:33 -0700

    first commit

--- rspec_exercise_5/lib/exercise.rb
+++ rspec_exercise_5/lib/exercise.rb
@@ -1,35 +1,29 @@
-def zip(*arr)
-    new_arr = []
-      i = 0
-    while i < arr[0].length
-      tmp_arr = []
-      j = 0
-      while j < arr.length
-        tmp_arr << arr[j][i]
-        j+=1
-      end
-      new_arr << tmp_arr
-      
-      i+=1
-    end
-    new_arr
-  end 
+# def zip(*arr)
+#   new_arr = Array.new(arr[0].length){Array.new(arr.length)}
+#   print new_arr
+#   arr[0].each_index do |col|
+#     arr.each_index do |row|
+#       new_arr[col][row] = arr[row][col]
+#     end
+#   end
+#   new_arr
+# end
 
+def zip(*arr)
+  new_arr = arr.transpose
+end
 
 def prizz_proc(arr, prc1, prc2)
-    new_arr = []
-    arr.each do |num|
-        if (prc1.call(num) || prc2.call(num)) && (!prc1.call(num) || !prc2.call(num))
-            new_arr << num
-        end
-    end
-    new_arr
+  new_arr = arr.select do |ele|
+    (prc1.call(ele) || prc2.call(ele)) && (!(prc1.call(ele)) || !(prc2.call(ele)))
+  end
 end
 
 def zany_zip(*arr)
+    length = max(arr)
     new_arr = []
     i = 0
-    while i < (arr.max).length
+    while i < length
         tmp = []
         j = 0
         while j < arr.length
@@ -42,46 +36,70 @@ def zany_zip(*arr)
     new_arr
 end
 
-def maximum(arr, &prc)
-    max = arr[0]
-    arr.each do |ele|
-        if  prc.call(ele) >= prc.call(max)
-            max = ele
+def max(arr)
+    max = 0
+    arr.each do |subarr|
+        if subarr.length > max
+            max = subarr.length
         end
     end
     max
+end
+
+def maximum(arr, &prc)
+  max = arr[0]
+  arr.each do |ele|
+    if prc.call(ele) >= prc.call(max)
+      max = ele
+    end
+  end
+  max
 end
 
 def my_group_by(arr, &prc)
-    hash = Hash.new {|h,k| h[k]= []}
-    arr.each do |ele|
-        hash[prc.call(ele)] << ele
-    end
-    hash 
+  hash = Hash.new { |h, k| h[k] = [] }
+  arr.each do |ele|
+    hash[prc.call(ele)] << ele
+  end
+  hash
 end
 
-# def max_tie_breaker(arr, breaker, &prc)
-#     max = arr[0]
-#     arr[1..-1].each do |ele|
-#         if prc.call(ele) > prc.call(max)
-#             max = ele
-#         else prc.call(ele) == prc.call(max) && breaker.call(ele) > breaker.call(max)
-#             max = ele
-#         end
-#     end
-#     max
-# end
-
-def max_tie_breaker(arr, breaker, &prc)
-    i = 1
-    max = arr[0]
-    while i < arr.length
-        if prc.call(arr[i]) > prc.call(max)
-            max = arr[i]
-        elsif prc.call(arr[i]) == prc.call(max) && breaker.call(arr[i]) > breaker.call(max)
-            max = arr[i]
-        end
-        i += 1
+def max_tie_breaker(arr, prc1, &prc2)
+  return nil if arr.empty?
+  largest = ""
+  arr.each do |ele|
+    if prc2.call(ele) > prc2.call(largest)
+      largest = ele
+    elsif prc2.call(ele) == prc2.call(largest) && prc1.call(ele) > prc1.call(largest)
+      largest = ele
     end
-    max
+  end
+  largest
+end
+
+def silly_syllables(sent)
+    new_arr = []
+  arr = sent.split(" ")
+  arr.each do |ele|
+    if indices(ele).length < 2
+        new_arr << ele
+    else
+        new_arr << update(ele)
+    end
+  end
+  new_arr.join(" ")
+end
+
+def indices(str)
+  vowels = "aeiouAeiou"
+  idx = []
+  str.each_char.with_index do |char, i|
+    idx << i if vowels.include?(char)
+  end
+  idx
+end
+
+def update(word)
+ id = indices(word)
+ word[id[0]..id[-1]]
 end
